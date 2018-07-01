04b66b80ea758cf58186ef2c3ff19f032968c340
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2018-07-01 22:21:49 +0100

    [ft-admin-sugnup 158667253] - adds user model

--- lib/routes.rb
+++ lib/routes.rb
@@ -1,12 +1,18 @@
+require 'bcrypt'
+
 class MicroLearning
-  get '/signup' do
+  get '/admin/signup' do
     haml :signup
   end
 
-  post '/signup' do
-        username = params[:username]
-        email = params[:email]
-        password = params[:password]
+  post "/admin/signup" do
+    username = params[:username]
+    email = params[:email]
+    password = params[:password]
+    password_salt = BCrypt::Engine.generate_salt
+    password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    @user = User.new(username: username, admin: true, email: email, password: password_hash)
+    @user.save
     haml :signup
   end
 end
