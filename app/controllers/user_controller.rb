3b976e77f5d2025e15579abc8c2dc52c5a64097f
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2018-07-04 16:54:35 +0100

    [ft-user-login 158667264] - impleemnt user login Finishes 158667264

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,4 +25,25 @@ class UserController < ApplicationController
         redirect to "/signup"
     end
   end
+
+  # User currently logged in will view the Courses page directly
+  get '/login' do
+    if logged_in?
+      redirect to "/topics"
+    else
+      erb :"/users/login"
+    end
+  end
+
+  # Verify user information to Log In
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/topics"
+    else
+      flash[:error] = "Sorry, we were not able to find a user with that username and password."
+      redirect to "/login"
+    end
+  end
 end
