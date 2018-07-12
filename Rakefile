d22728689fc31cad0ac32a8e8759000a2cfe326b
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2018-07-12 18:14:42 +0100

    amend rakefile config

--- Rakefile
+++ Rakefile
@@ -1,3 +1,2 @@
-ENV["SINATRA_ENV"] ||= "development"
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180706141543) do
+ActiveRecord::Schema.define(version: 2018_07_06_141543) do
 
   create_table "resources", force: :cascade do |t|
     t.string "title"
@@ -35,9 +34,9 @@ ActiveRecord::Schema.define(version: 20180706141543) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "email"
-    t.string  "password_digest"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.boolean "admin"
   end
 
