079e03cc6daa9ec25742ebe2eb1e53ef4e316e63
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2018-07-20 10:28:22 +0100

    [ch-write-test-159115085] -write test for topic controller #159115085

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,16 @@ ENV["RACK_ENV"] = "test"
 require 'rack/test'
 require 'rspec'
 require_relative '../config/environment'
-
+require 'capybara/rspec'
+require 'capybara/dsl'
 
 # remove loggers
 ActiveRecord::Base.logger = nil
 
 module RSpecMixin
   include Rack::Test::Methods
+  include Capybara::DSL
+
   DatabaseCleaner.strategy = :truncation
   def app
     described_class
@@ -26,4 +29,11 @@ RSpec.configure do |config|
   config.after do
     DatabaseCleaner.clean
   end
+  config.order = 'default'
 end
+
+def app
+  Rack::Builder.parse_file('config.ru').first
+end
+
+Capybara.app = app
